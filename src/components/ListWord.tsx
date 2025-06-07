79f154fe246a3f5af82193acc429a21e9ee7bd53
lazalajochy <lazala1401jochy@outlook.com>
2025-06-07 17:21:41 -0400

    remove unnecesary importo

--- src/components/ListWord.tsx
+++ src/components/ListWord.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import request from '../utils/request';
 import { RootState } from "../store";
