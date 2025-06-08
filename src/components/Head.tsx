6279545547279b094034ee64dfff0c92fed4e104
lazalajochy <lazala1401jochy@outlook.com>
2025-06-07 23:02:03 -0400

    font-size

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -11,12 +11,13 @@ export const Head = () => {
             <div className="max-w-7xl mx-auto flex flex-col sm:flex-row items-center justify-between px-10 h-full">
 
                 <div className="flex flex-col md:flex-row lg:flex-row xl:flex-row 2xl:flex-row items-center gap-x-10 gap-y-4">
-                    <div className="flex items-center gap-2 transition duration-300 ease-in-out hover:scale-105 cursor-pointer">
+
+                    <Link  to="/dashboard/vacabulary" className="flex items-center gap-2 transition duration-300 ease-in-out hover:scale-105 cursor-pointer">
                         <MdLibraryBooks className="text-white text-2xl transition duration-300 ease-in-out hover:text-gray-300" />
                         <h2 className="text-white font-bold text-2xl hover:text-gray-300 transition duration-300 ease-in-out">Vocabulary</h2>
+                    </Link>
 
 
-                    </div>
                     <Link to="/dashboard/listWord" className="flex items-center gap-2  transition duration-300 ease-in-out hover:scale-105 cursor-pointer">
                         <MdList className="text-white text-2xl transition duration-300 ease-in-out hover:text-gray-300" />
                         <h2 className="text-white font-bold text-2xl hover:text-gray-300 transition duration-300 ease-in-out">Vocabulary List</h2>
