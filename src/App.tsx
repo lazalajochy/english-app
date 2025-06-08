e274549114cce13d64d5fcdba94f6c371b84b127
lazalajochy <lazala1401jochy@outlook.com>
2025-06-07 20:09:06 -0400

    routes back

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+import { MainRoute } from './routes/MainRoute';
 
 function App() {
   return (
-    <div>
-      
-      <Outlet />
-    </div>
+    <GoogleOAuthProvider clientId="1044435155755-s32q81p9sho3nsi32r6mn7n16g16p39k.apps.googleusercontent.com">
+      <MainRoute />
+    </GoogleOAuthProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 import { Provider } from 'react-redux';
-import store from "./store.ts";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { MainRoute } from './routes/MainRoute';
+import store from "./store.ts"
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId="1044435155755-s32q81p9sho3nsi32r6mn7n16g16p39k.apps.googleusercontent.com">
-        <MainRoute />
-      </GoogleOAuthProvider>
+      <App />
     </Provider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
